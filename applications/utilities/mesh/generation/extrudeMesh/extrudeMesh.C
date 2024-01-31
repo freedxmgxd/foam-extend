b68cc47c11b4f59d12d25d0d1492cfb5a09b34e3
Hrvoje Jasak <h.jasak@wikki.co.uk>
2024-01-31 09:02:43 +0000

    Changed name of extrudeMesh dictionary

--- applications/utilities/mesh/generation/extrudeMesh/extrudeMesh.C
+++ applications/utilities/mesh/generation/extrudeMesh/extrudeMesh.C
@@ -66,8 +66,8 @@ int main(int argc, char *argv[])
     (
         IOobject
         (
-            "extrudeProperties",
-            runTimeExtruded.constant(),
+            "extrudeMeshDict",
+            runTimeExtruded.system(),
             runTimeExtruded,
             IOobject::MUST_READ
         )
