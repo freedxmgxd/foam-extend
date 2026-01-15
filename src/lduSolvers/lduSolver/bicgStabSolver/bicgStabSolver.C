e4b9f3ab0f3058019899a187f01ddecc5f10a51a
Hrvoje Jasak <h.jasak@wikki.co.uk>
2026-01-15 12:14:34 +0000

    Revert to AMul precontioning in bicgStabSolver

--- src/lduSolvers/lduSolver/bicgStabSolver/bicgStabSolver.C
+++ src/lduSolvers/lduSolver/bicgStabSolver/bicgStabSolver.C
@@ -173,9 +173,8 @@ Foam::lduSolverPerformance Foam::bicgStabSolver::solve
 
             // Execute preconditioning
             // Bug fix, Alexander Monakov, 11/Jul/2012
-            // This is wrong: reverting to T product.  HJ, 27/Jun/2025
-            preconPtr_->preconditionT(sh, s, cmpt);
-            matrix_.Tmul(t, sh, coupleBouCoeffs_, interfaces_, cmpt);
+            preconPtr_->precondition(sh, s, cmpt);
+            matrix_.Amul(t, sh, coupleBouCoeffs_, interfaces_, cmpt);
 
             scalar tt = gSumProd(t, t);
 
